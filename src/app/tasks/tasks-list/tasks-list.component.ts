3199f12c9f25cbf3bb28f0ece33f0e6aff7e7d07
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-18 15:19:51 +0300

    chore: add initial configuration and setup files

---
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+
+import { TaskItemComponent } from './task-item/task-item.component';
+import { Task } from '../task.model';
+
+@Component({
+  selector: 'app-tasks-list',
+  standalone: true,
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css',
+  imports: [TaskItemComponent],
+})
+export class TasksListComponent {
+  selectedFilter = signal<string>('all');
+  tasks: Task[] = [];
+
+  onChangeTasksFilter(filter: string) {
+    this.selectedFilter.set(filter);
+  }
+}
